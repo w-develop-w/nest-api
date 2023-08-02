bce1813319b33a6b76e0cb689e933e0210ea8d8d
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-03 00:59:11 +0300

    adding first decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('test')
+  getHelloooo(): string {
+    console.log('Get message');
+    // return this.appService.getHello();
+    return 'hello world';
+  }
+
+  @Post('create')
+  create() {
+    console.log('post');
   }
 }
