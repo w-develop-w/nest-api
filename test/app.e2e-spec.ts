c23330c817eedb0666d049390081ef5f331a9047
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-03 05:46:09 +0300

    tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,12 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/app/create (POST)', async () => {
+    const res = await request(app.getHttpServer()).post('/app/create').send({
+      num: 1,
+    });
+    expect(res.body.num).toBe(1);
+    expect(res.body.id).toBeGreaterThan(0);
+    expect(res.statusCode).toBe(201);
   });
 });
